0cb419314be179288a4bddb2f119b01e0c899f20
kiran <kiran@neostack.com>
2017-06-24 11:11:36 +0530

    code splitting

--- src/app.js
+++ src/app.js
@@ -2,11 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './components/Button'
 import userIcon from './images/user.png'
-import Success from './pages/success'
-import Warning from './pages/warning'
-import Error from './pages/error'
+// import Success from './pages/success'
+// import Warning from './pages/warning'
+// import Error from './pages/error'
 import { Router, Route, browserHistory, Link } from 'react-router'
 import './app.css'
+
+function getSuccess(nextState, callback){
+  require.ensure([], (require) => {
+    callback(null, require('./pages/success').default)
+  });
+}
+function getWarning(nextState, callback){
+  require.ensure([], (require) => {
+    callback(null, require('./pages/warning').default)
+  });
+}
+function getError(nextState, callback){
+  require.ensure([], (require) => {
+    callback(null, require('./pages/error').default)
+  });
+}
 
 export default class App extends React.Component {
   render() {
@@ -30,9 +46,9 @@ export default class App extends React.Component {
 const routes = (
   <Router history={browserHistory}>
     <Route path='/' component={App}>
-      <Route path='/success' component={Success} />
-      <Route path='/warning' component={Warning} />
-      <Route path='/error' component={Error} />
+      <Route path='/success' getComponent={getSuccess} />
+      <Route path='/warning' getComponent={getWarning} />
+      <Route path='/error' getComponent={getError} />
     </Route>
   </Router>
 )
