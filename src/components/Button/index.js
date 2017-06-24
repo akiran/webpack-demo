25bbc42985498078e344a295e2737518c09876fe
kiran <kiran@neostack.com>
2017-06-24 11:25:22 +0530

    tree-shaking

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import classnames from 'classnames'
 import './button.css'
 
+export function LargeButton(props) {
+  return <Button>{props.children}</Button>
+}
+
 export default function Button(props) {
-  const className = classnames('button', {
+  const className = classnames(props.className||'', 'button', {
     'button-success': props.type === 'success',
     'button-warning': props.type === 'warning',
     'button-error': props.type === 'error'
