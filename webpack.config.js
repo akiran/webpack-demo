0661492473778700ad2bd13ec29c6457a0172b2b
kiran <kiran@neostack.com>
2017-06-24 07:57:40 +0530

    basic setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path')
+
+module.exports = function(env) {
+  console.log(env)
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'app.js'
+    },
+    module: {
+      rules: [
+        {test: /\.js$/, use: 'babel-loader'},
+      ]
+    }
+  }
+}
